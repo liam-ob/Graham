e60430e76d676fef191439b3c13d7e179b27e6ba
liam-ob <98386642+liam-ob@users.noreply.github.com>
2022-03-22 11:22:15 +0800

    websocket and momentum implementation

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import hmac
+import json
+import time
+import pprint
 import websocket
+
+
 # import time
 # import hmac
 # from requests import Request
 
 def main():
-
     # ts = int(time.time() * 1000)
     # request = Request('GET', 'https://ftx.com/api')
     # prepared = request.prepare()
@@ -20,19 +25,49 @@ def main():
     #
     # print(prepared.headers)
 
-    SOCKET = "wss://ftx.com/ws/trades"
+    SOCKET = "wss://ftx.com/ws/{'op': 'pong'}"
+
+    api_key = "mwUORjWwcsCQ5czmCFXqXQx9Kl1_AX9pT0KIltmh"
+    secret_key = "MLvuKeb0xgfC2O6RlCxhOIrHuTxYs_LZ0NgSimir"
+    trade_size = 10000
 
     def on_open(ws):
         print("opened connection\n\n")
-        print(ws)
+        ts = int(time.time() * 1000)
+        signa = hmac.new(secret_key.encode(), f'{ts}websocket_login'.encode(), 'sha256').hexdigest()
+        auth = {'op': 'login', 'args': {'key': api_key,
+                                        'sign': signa,
+                                        'time': ts}}
+        ws.send(json.dumps(auth))
+        data = {'op': 'subscribe', 'channel': 'trades', 'market': 'BTC-PERP'}
+        ws.send(json.dumps(data))
 
     def on_close(ws):
         print("\nwebsocket closed!\n")
 
     def on_message(ws, message):
-        print(message)
+        print("\n")
+        json_message = json.loads(message)
+        pprint.pprint(json_message)
+
+        market_momentum = 0
+
+        data = json_message['data']
+        for i in data:
+            buy_or_sell = i['side']
+            weight = (i['price'] * i['size'])
+            if weight > trade_size:
+                if buy_or_sell == 'buy':
+                    market_momentum += weight
+                    print('\t\t\t\t\tmomentum going up')
+                else:
+                    market_momentum -= weight
+                    print('\t\t\t\t\tmarket momentum going down')
+
+        print(market_momentum)
+
     ws = websocket
-    ws = ws.WebSocketApp(SOCKET, on_open=on_open,on_message=on_message, on_close=on_close)
+    ws = ws.WebSocketApp(SOCKET, on_open=on_open, on_message=on_message, on_close=on_close)
     ws.run_forever()
 
 
